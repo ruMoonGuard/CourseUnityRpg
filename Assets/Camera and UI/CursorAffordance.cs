671693b06d6c09715b5981eb4e87562d13dc1ee7
Yaroslav <ufamoonguard@gmail.com>
2017-12-22 00:59:58 +0500

    structuring
    structuring

---
+++ Assets/Camera and UI/CursorAffordance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D _walkCursor = null;
+    [SerializeField] Texture2D _attackCursor = null;
+    [SerializeField] Texture2D _stopCursor = null;
+
+    [SerializeField] Vector2 hotspot = new Vector2(96, 96);
+
+    public CameraRaycat _cameraRaycat;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if(_cameraRaycat == null)
+        {
+            _cameraRaycat = GetComponent<CameraRaycat>();
+        }
+	}
+
+    private void Update()
+    {
+        switch (_cameraRaycat.LayerHit)
+        {
+            case Layers.Enemy:
+                Cursor.SetCursor(_attackCursor, hotspot, CursorMode.Auto);
+                break;
+            case Layers.Walkable:
+                Cursor.SetCursor(_walkCursor, hotspot, CursorMode.Auto);
+                break;
+            default:
+                Cursor.SetCursor(_stopCursor, hotspot, CursorMode.Auto);
+                break;
+        }
+    }
+}
